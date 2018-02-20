ded05d983586a1b51b2e06d8bf795a6425a9bbf3
AnthonyEisenback <anthonyeisenback@Interapts-MacBook-Pro-3.local>
2018-02-20 13:50:40 -0500

    Added a blank switch statement

---
+++ src/com/company/Menu.java
@@ -0,0 +1,50 @@
+package com.company;
+import java.util.Scanner;
+
+public class Menu {
+    private Scanner scanner = new Scanner(System.in);
+
+    protected void Options() {
+
+        System.out.println("What would you like to do today?\n 1.)Add a task \n" +
+                "2.) Remove a task\n" +
+                "3.) Uncompleted tasks\n" +
+                "4.) Completed Tasks\n " +
+                "5.) Edit a task \n " +
+                "6.) View details of a task\n" +
+                "7.) Exit Program ");
+
+        switch (scanner.nextInt()) {
+
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            case 5:
+                break;
+
+            case 6:
+                break;
+
+            case 7:
+                break;
+
+                default:
+                    System.out.println("Please enter a number");
+                    break;
+
+
+
+        }
+
+
+    }
+}
