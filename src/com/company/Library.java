a950f2601ed5299255373013cc25824064386c53
AnthonyEisenback <anthonyeisenback@Interapts-MacBook-Pro-3.local>
2018-02-20 13:55:41 -0500

    Added Library class and sub methods

---
+++ src/com/company/Library.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Library {
+
+    private void viewTasks ()  {
+
+    }
+
+    private void addTask () {
+
+    }
+
+    private void removeTask () {
+
+    }
+
+    private void exit (){
+        System.out.println("Goodbye");
+        System.exit(0);
+    }
+
+    private void editTask () {
+
+    }
+
+    private void selectAndViewDetail () {
+
+    }
+
+    private void removeUncompletedTask () {
+
+    }
+
+    private void time () {
+        
+    }
+}
